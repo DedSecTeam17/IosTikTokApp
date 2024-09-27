e33af520c383eb398b3c4d2ccd26a2f98604ac2e
Mohammed <melamin100@yahoo.com>
2024-09-27 09:18:41 +0400

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Mohammed Elamin on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
